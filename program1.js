779d57c33a8fc57939bd8b9d148bc1c0d7b5d1ba
Mohammed Faraz Uddin <mohammedfarazuddin01@gmail.com>
2024-11-11 15:40:45 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,27 +1,23 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
-    const bracketMap={
-        ')':'(',
-        '}':'{',
-        ']':'['
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
     };
-    const stack=[];
-    for (let char of s){
-        if( bracketMap[char]){
-            const topElement =stack.length>0 ? stack.pop(): '#';
-            if(bracketMap[char]!==topElement){
+    const stack = [];
+    
+    for (let char of s) {
+        if (bracketMap[char]) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (bracketMap[char] !== topElement) {
                 return false;
             }
-        }else{
+        } else {
             stack.push(char);
         }
-        return stack.length ===0;
     }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
