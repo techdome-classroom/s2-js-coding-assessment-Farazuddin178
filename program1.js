9538dc445a3b29d3eec883bde080073dcbf2ca62
Mohammed Faraz Uddin <mohammedfarazuddin01@gmail.com>
2024-11-11 15:19:14 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,7 +11,7 @@ var isValid = function(s) {
     const stack=[];
     for (let char of s){
         if(char in bracketMap){
-            
+            const topElement =stack.length>
         }
     }
 };
